881c2a701618d1c9d827e2b156978d39c168daf7
lucasxas <lucasxas@gmail.com>
2015-07-16 23:13:46 +1200

    Randomly creates a Pirate and populates the views using Data Binding Library.

--- app/src/main/java/gdg/wellington/databinding/MainActivity.java
+++ app/src/main/java/gdg/wellington/databinding/MainActivity.java
@@ -1,14 +1,56 @@
 package gdg.wellington.databinding;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Random;
+
+import gdg.wellington.databinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends Activity {
+
+    String[] names = {"Anne", "Bette", "Cate",
+            "Dawn", "Elise", "Faye", "Ginger",
+            "Harriot", "Izzy", "Jane", "Kaye",
+            "Liz", "Maria", "Nell", "Olive",
+            "Pat", "Queenie", "Rae", "Sal",
+            "Tam", "Uma", "Violet", "Wilma",
+            "Xana", "Yvonne", "Zelda",
+            "Abe", "Billy", "Caleb", "Davie",
+            "Eb", "Frank", "Gabe", "House",
+            "Icarus", "Jack", "Kurt", "Larry",
+            "Mike", "Nolan", "Oliver", "Pat",
+            "Quib", "Roy", "Sal", "Tom",
+            "Ube", "Val", "Walt", "Xavier",
+            "Yvan", "Zeb"};
+    String[] appellations = {"Awesome", "Captain",
+            "Even", "Fighter", "Great", "Hearty",
+            "Jackal", "King", "Lord", "Mighty",
+            "Noble", "Old", "Powerful", "Quick",
+            "Red", "Stalwart", "Tank", "Ultimate",
+            "Vicious", "Wily", "aXe", "Young",
+            "Brave", "Eager", "Kind", "Sandy",
+            "Xeric", "Yellow", "Zesty"};
+
+    private ActivityMainBinding binding;
+
+    private Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+
+    public void onClickNewNameButton(View view) {
+        String name = names[random.nextInt(names.length)];
+        String appellation = this.appellations[random.nextInt(this.appellations.length)];
+
+        Pirate pirate = new Pirate(name, appellation);
+        binding.setPirate(pirate);
     }
 
 }
---
+++ app/src/main/java/gdg/wellington/databinding/Pirate.java
@@ -0,0 +1,10 @@
+package gdg.wellington.databinding;
+
+public class Pirate {
+    public final String name;
+    public final String appellation;
+    public Pirate(String name, String appellation) {
+        this.name = name;
+        this.appellation = appellation;
+    }
+}
